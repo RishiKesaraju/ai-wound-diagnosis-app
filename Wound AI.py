fa0dfa00c0a5ba27f35132253cc4019876613c92
Rishi Kesaraju <rishi.kesaraju@gmail.com>
2024-11-28 20:39:32 -0800

    Updated requirements.txt and fixed Python version compatibility

--- Wound AI.py
+++ Wound AI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.utils import load_img, img_to_array
 import numpy as np
 from openai import OpenAI
 import pandas as pd
